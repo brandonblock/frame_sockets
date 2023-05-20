7b10634e0af80a3c582f08ee791cffaf46a23808
Brandon Block <brandon.block@pm.me>
2023-05-20 17:31:12 -0500

    main fn to send buffer into ws handler

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,15 +1,36 @@
 use futures_util::stream::SplitSink;
 use futures_util::stream::SplitStream;
+use futures_util::StreamExt;
+use std::sync::{Arc, Mutex};
+use tokio::net::TcpListener;
+use tokio_tungstenite::WebSocketStream;
 use tungstenite::protocol::Message;
-use tungstenite::WebSocket;
 
 //window dimensions (SD)
 const WIDTH: usize = 640;
 const HEIGHT: usize = 480;
 
-type WsSink = SplitSink<WebSocket<tokio::net::TcpStream>, Message>;
-type WsStream = SplitStream<WebSocket<tokio::net::TcpStream>>;
+type WsSink = SplitSink<WebSocketStream<tokio::net::TcpStream>, Message>;
+type WsStream = SplitStream<WebSocketStream<tokio::net::TcpStream>>;
 
-fn main() {
-    println!("Hello, world!");
+async fn handle_client(mut sink: WsSink, mut stream: WsStream, framebuffer: Arc<Mutex<Vec<u32>>>) {
+    todo!();
+}
+
+#[tokio::main]
+async fn main() {
+    // letting this unwrap() because if it fails, there's no point in continuing
+    let listener = TcpListener::bind("127.0.0.1:9001").await.unwrap();
+
+    let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
+    let buffer = Arc::new(Mutex::new(buffer));
+
+    loop {
+        if let Ok((stream, _)) = listener.accept().await {
+            //TODO: handle unwrap
+            let ws_stream = tokio_tungstenite::accept_async(stream).await.unwrap();
+            let (sink, stream) = ws_stream.split();
+            tokio::spawn(handle_client(sink, stream, Arc::clone(&buffer)));
+        }
+    }
 }
