c2c8e004b6aa8aed2eb5304704f311012b037b71
Brandon Block <brandon.block@pm.me>
2023-05-20 17:18:32 -0500

    initial types for websocket conn

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,15 @@
+use futures_util::stream::SplitSink;
+use futures_util::stream::SplitStream;
+use tungstenite::protocol::Message;
+use tungstenite::WebSocket;
+
+//window dimensions (SD)
+const WIDTH: usize = 640;
+const HEIGHT: usize = 480;
+
+type WsSink = SplitSink<WebSocket<tokio::net::TcpStream>, Message>;
+type WsStream = SplitStream<WebSocket<tokio::net::TcpStream>>;
+
 fn main() {
     println!("Hello, world!");
 }
