8a5604af7fe5e76684c23e31eae0a62cc78df928
Brandon Block <brandon.block@pm.me>
2023-05-20 17:55:15 -0500

    send empty frame-like objects on socket connection

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,5 +1,7 @@
+use byteorder::{ByteOrder, LittleEndian};
 use futures_util::stream::SplitSink;
 use futures_util::stream::SplitStream;
+use futures_util::SinkExt;
 use futures_util::StreamExt;
 use std::sync::{Arc, Mutex};
 use tokio::net::TcpListener;
@@ -14,7 +16,19 @@ type WsSink = SplitSink<WebSocketStream<tokio::net::TcpStream>, Message>;
 type WsStream = SplitStream<WebSocketStream<tokio::net::TcpStream>>;
 
 async fn handle_client(mut sink: WsSink, mut stream: WsStream, framebuffer: Arc<Mutex<Vec<u32>>>) {
-    todo!();
+    while let Some(Ok(_msg)) = stream.next().await {
+        // assign bytes in this scope to drop the framebuffer lock as soon as possible
+        let bytes = {
+            let framebuffer = framebuffer.lock().unwrap();
+            let mut bytes = vec![0; framebuffer.len() * 4];
+            LittleEndian::write_u32_into(&framebuffer, &mut bytes);
+            bytes
+        };
+        let msg = Message::binary(bytes);
+        if (sink.send(msg).await).is_err() {
+            break;
+        }
+    }
 }
 
 #[tokio::main]
@@ -22,8 +36,7 @@ async fn main() {
     // letting this unwrap() because if it fails, there's no point in continuing
     let listener = TcpListener::bind("127.0.0.1:9001").await.unwrap();
 
-    let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
-    let buffer = Arc::new(Mutex::new(buffer));
+    let buffer = Arc::new(Mutex::new(vec![0; WIDTH * HEIGHT]));
 
     loop {
         if let Ok((stream, _)) = listener.accept().await {
